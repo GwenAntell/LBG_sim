e5a2e1ce11c5bc340f252551232e038a65ea5f09
Gwen Antell <gwenantell@gmail.com>
2022-03-14 15:33:42 +0000

    reduce n reps from 1000 to 500 for quicker results

--- R/options.R
+++ R/options.R
@@ -1,7 +1,8 @@
 #options
 libs = c("raster", "plyr", "dplyr", "stringr", "ggplot2", "ggpubr", "grid", "png", "viridis", "gridExtra") #libraries
 res = 1 #spatial resolution of the study
-reps = 100 #number of replications for simulations
+reps = # 100 #number of replications for simulations
+  50 # 100 reps of 1k species takes a LONG time
 species = 1000 #number of species to generate in simulations
 lat_bin = 15 #latitudinal bin size for analyses
 rarefy_sample = 50 #sample size for sample-sized rarefaction
